3e4ed0419de4f41246c35424e81ce685957a4f03
gmallard <allard.guy.m@gmail.com>
2012-05-02 08:46:49 -0400

    Use symbols, not strings for header keys (issue #35).

--- lib/stomp/connection.rb
+++ lib/stomp/connection.rb
@@ -623,8 +623,8 @@ module Stomp
           # and ActiveMQ will interpret the message as a TextMessage.
           # For more information refer to http://juretta.com/log/2009/05/24/activemq-jms-stomp/
           # Lets send this header in the message, so it can maintain state when using unreceive
-          headers['content-length'] = "#{body_length_bytes}" unless headers[:suppress_content_length]
-          headers['content-type'] = "text/plain; charset=UTF-8" unless headers['content-type']
+          headers[:'content-length'] = "#{body_length_bytes}" unless headers[:suppress_content_length]
+          headers[:'content-type'] = "text/plain; charset=UTF-8" unless headers[:'content-type']
           used_socket.puts command
           headers.each do |k,v|
             if v.is_a?(Array)
