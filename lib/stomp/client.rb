74742229c2ee302353c6ec3f0acc585706e94dff
gmallard <allard.guy.m@gmail.com>
2014-08-10 12:29:42 -0400

    Change start_timeout default to zero.

--- lib/stomp/client.rb
+++ lib/stomp/client.rb
@@ -82,7 +82,7 @@ module Stomp
         parse_positional_params(login, passcode, host, port, reliable)
 
       @logger = @parameters[:logger] ||= Stomp::NullLogger.new
-      @start_timeout = @parameters[:start_timeout] || 10.0
+      @start_timeout = @parameters[:start_timeout] || 0
       check_arguments!()
 
       begin
